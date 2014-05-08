5bb5ea706ae6b9d4615d9c943d6004042fc794f2
angelbirth <ricky.ason@gmail.com>
2014-05-08 15:04:03 +0700

    added state machine

--- src/mobileapplication1/MyCanvas.java
+++ src/mobileapplication1/MyCanvas.java
@@ -5,26 +5,32 @@
  */
 package mobileapplication1;
 
+import java.util.Random;
 import javax.microedition.lcdui.*;
-import javax.microedition.lcdui.game.GameCanvas;
 
 /**
  * @author ric
  */
-public class MyCanvas extends GameCanvas implements Runnable {
+public class MyCanvas extends Canvas implements Runnable {
 
+    private int _nextState;
     private static final int FPS = 60;
+    private int _state;
+    private boolean _exit = false;
+    private boolean s_stateEnterNext = false;
+    private boolean s_stateExitCurrent = false;
+    private Graphics _g;
+    private int _counter;
 
     /**
      * constructor
      */
     private MyCanvas() {
-        super(false);
-        x = y = 0;
-        dx = dy = 6;
+        super();
+        stateSwitch(Constants.STATE_GAMEPLAY);
     }
 
-    public static Canvas getInstance() {
+    public static MyCanvas getInstance() {
         return CanvasHolder.INSTANCE;
     }
     private int x, y;
@@ -35,62 +41,41 @@ public class MyCanvas extends GameCanvas implements Runnable {
      * paint
      */
     public void paint(Graphics g) {
+        _g = g;
+        _counter++;
+        StateMachine_sendMessage(Constants.MESSAGE_PAINT);
+    }
 
+    private void draw(Graphics g) {
         g.setColor(0);
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(0x00ff00ff);
         g.fillRect(x, y, 40, 40);
-
     }
 
-    /**
-     * Called when a key is pressed.
-     */
-    protected void keyPressed(int keyCode) {
-    }
+    public void StateMachine_Update() {
+        if (s_stateExitCurrent) {
+            StateMachine_sendMessage(Constants.MESSAGE_DESTROY);
+            s_stateExitCurrent = false;
+            s_stateEnterNext = true;
+            _state = _nextState;
+            _nextState = -1;
+        }
 
-    /**
-     * Called when a key is released.
-     */
-    protected void keyReleased(int keyCode) {
-    }
-
-    /**
-     * Called when a key is repeated (held down).
-     */
-    protected void keyRepeated(int keyCode) {
-    }
-
-    /**
-     * Called when the pointer is dragged.
-     */
-    protected void pointerDragged(int x, int y) {
-    }
-
-    /**
-     * Called when the pointer is pressed.
-     */
-    protected void pointerPressed(int x, int y) {
-    }
-
-    /**
-     * Called when the pointer is released.
-     */
-    protected void pointerReleased(int x, int y) {
+        if (s_stateEnterNext) {
+            s_stateEnterNext = false;
+            _counter = 0;
+            StateMachine_sendMessage(Constants.MESSAGE_INIT);
+        }
+        StateMachine_sendMessage(Constants.MESSAGE_UPDATE);
     }
 
     public void run() {
-        while (true) {
+        while (!_exit) {
             startTime = System.currentTimeMillis();
 
-            if (x > getWidth() - 40 || x < 0) {
-                dx = -dx;
-            }
-            if (y > getHeight() - 40 || y < 0) {
-                dy = -dy;
-            }
-            x += dx;
-            y += dy;
+            StateMachine_Update();
+
             endTime = System.currentTimeMillis();
             long delta = (1000 / FPS) - (endTime - startTime);
             System.out.println(delta);
@@ -100,10 +85,56 @@ public class MyCanvas extends GameCanvas implements Runnable {
             try {
                 Thread.sleep(delta);
             } catch (InterruptedException ex) {
-                ex.printStackTrace();
             }
             repaint();
         }
+    }
+
+    protected void stateSwitch(int nextState) {
+        if (nextState == -1) {
+            return;
+        }
+        _nextState = nextState;
+        s_stateExitCurrent = true;
+    }
+
+    public void StateMachine_sendMessage(int msg) {
+        switch (_state) {
+            case Constants.STATE_LOGO:
+
+                break;
+            case Constants.STATE_GAMEPLAY:
+                updateGameplay(msg);
+                break;
+        }
+    }
+
+    private void updateGameplay(int msg) {
+        switch (msg) {
+            case Constants.MESSAGE_UPDATE: {
+                if (x > getWidth() - 40 || x < 0) {
+                    dx = -dx;
+                }
+                if (y > getHeight() - 40 || y < 0) {
+                    dy = -dy;
+                }
+                x += dx;
+                y += dy;
+            }
+            break;
+            case Constants.MESSAGE_PAINT:
+                draw(_g);
+                break;
+            case Constants.MESSAGE_INIT:
+                x = y = 100;
+                dx = dy = 6;
+            default:
+                break;
+        }
+    }
+
+    private void updateLogo(int message) {
+
     }
 
     private static final class CanvasHolder {
